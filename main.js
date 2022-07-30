26c28044462b93dcd20f8a0239fb7680e40b4fa8
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-07-30 22:58:27 +0430

    javascript settings added to external file

--- main.js
+++ main.js
@@ -22,3 +22,39 @@ navToggle.addEventListener('click',()=>{
     primaryNav.toggleAttribute('data-visible');
     primaryHeader.toggleAttribute('data-visible')
 });
+
+$(document).ready(function () {
+    $('.carousel').slick({
+      slidesToShow: 3,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      arrows: false,
+      mobileFirst: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 100,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true
+          }
+        }
+      ]
+
+    });
+  });
