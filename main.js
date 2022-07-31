c8c51abdb58d9787de05e72f9478d02cb2790a74
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-07-31 09:18:11 +0430

    background shape image added

--- main.js
+++ main.js
@@ -2,20 +2,20 @@ const primaryHeader = document.querySelector('.primary-header');
 const navToggle = document.querySelector('.mobile-nav-toggle');
 const primaryNav = document.querySelector('.primary-navigation');
 
-const icon_Humburger = document.querySelector('.icon-humburger');
+const icon_Hamburger = document.querySelector('.icon-Hamburger');
 const icon_close = document.querySelector('.icon-close');
 
 navToggle.addEventListener('click',()=>{
 
     if(primaryNav.hasAttribute('data-visible') ){
         navToggle.setAttribute('aria-expanded',false);
-        icon_Humburger.style.display = 'block';
+        icon_Hamburger.style.display = 'block';
         icon_close.style.display = 'none';
         
 
     } else {
         navToggle.setAttribute('aria-expanded',true);
-        icon_Humburger.style.display = 'none';
+        icon_Hamburger.style.display = 'none';
         icon_close.style.display = 'block';
     }
 
